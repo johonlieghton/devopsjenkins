2d01b2a3fba4c2c06abfee3b1a2fb8df7ae8aac9
johon.lieghton@estudiantes.utec.edu.uy <johon.lieghton@estudiantes.utec.edu.uy>
2021-08-26 12:18:15 -0300

    prueba de jenkins

---
+++ ejecutar_constructor_script.sh
@@ -0,0 +1,4 @@
+#! /bash/bash
+
+echo "Se esta ejecutando el script demostracion del pipeline"
+touch target/demoapp.jar
---
+++ script.sh
@@ -0,0 +1,8 @@
+echo "#dockerjenkins" >> README.md
+git init
+git add README.md
+git commit -m "realizando el primer commit"
+git remote add origin
+git@github.com:johonlieghton/johonlieghton.git
+git push -u origin master
+
